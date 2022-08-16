65420309747302dae7e5e37ffa2ff95008f34d52
Bea <bealopezm91@gmail.com>
2022-08-16 23:54:05 +0200

    wine update

--- models/wine.model.js
+++ models/wine.model.js
@@ -7,12 +7,12 @@ const getById = (id) => {
   return executeQueryOne('select * from wine where id = ?', [id]);
 };
 
-const create = ({ name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id, Grape_id }) => {
-  return executeQuery('insert into wine (name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id, Grape_id) values (?,?,?,?,?,?,?)', [name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id, Grape_id]);
+const create = ({ name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id }) => {
+  return executeQuery('insert into wine (name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id) values (?,?,?,?,?,?,?)', [name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id]);
 };
 
-const update = (pId, { name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id, Grape_id }) => {
-  return executeQuery('update wine set name = ?, elaborationArea = ?, photo = ?, Origin_id = ?, WineCellar_id = ?, Type_id = ?, Grape_id = ? where id = ?', [name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id, Grape_id, pId]);
+const update = (pId, { name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id }) => {
+  return executeQuery('update wine set name = ?, elaborationArea = ?, photo = ?, Origin_id = ?, WineCellar_id = ?, Type_id = ? where id = ?', [name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id, pId]);
 };
 
 module.exports = {
