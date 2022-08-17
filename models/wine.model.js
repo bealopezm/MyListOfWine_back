1b689fa0268309429174c950863b8e1a5c441784
Bea <bealopezm91@gmail.com>
2022-08-17 14:20:30 +0200

    wineModel and wineHasUser updated

--- models/wine.model.js
+++ models/wine.model.js
@@ -1,10 +1,10 @@
 const { executeQuery, executeQueryOne } = require('../helpers/utils');
 
 const getAll = () => {
-  return executeQuery('select * from wine');
+  return executeQuery('SELECT wine.id, wine.name, wine.elaborationArea, wine.photo, origin.type as origin, origin.name as nameOrigin, type.name as type, winecellar.name as nameWineCellar  FROM wine join origin on origin.id = wine.Origin_id join type on type.id = wine.Type_id join winecellar on winecellar.id = wine.WineCellar_id');
 };
 const getById = (id) => {
-  return executeQueryOne('select * from wine where id = ?', [id]);
+  return executeQueryOne('SELECT wine.id, wine.name, wine.elaborationArea, wine.photo, origin.type as origin, origin.name as nameOrigin, type.name as type, winecellar.name as nameWineCellar  FROM wine join origin on origin.id = wine.Origin_id join type on type.id = wine.Type_id join winecellar on winecellar.id = wine.WineCellar_id where wine.id = ?', [id]);
 };
 
 const create = ({ name, elaborationArea, photo, Origin_id, WineCellar_id, Type_id }) => {
