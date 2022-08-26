1a370e21884884c38f54a20ccb40bea3d1eebc62
Bea <bealopezm91@gmail.com>
2022-08-26 16:08:19 +0200

    images updated

--- routes/api/wine.js
+++ routes/api/wine.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const multer = require('multer');
+const upload = multer({ dest: 'public/images' });
+const fs = require('fs');
 
 const { verifyToken } = require('../../helpers/middlewares');
 const { getAll, create, getById, update, getByName } = require('../../models/wine.model');
@@ -22,7 +25,15 @@ router.get('/:id', verifyToken, (req, res) => {
     .catch(err => res.json({ error: err.message }))
 });
 
-router.post('/', verifyToken, async (req, res) => {
+router.post('/', verifyToken, upload.single('photo'), async (req, res) => {
+  console.log(req.file)
+  const file = req.file.mimetype.split('/')[1];
+  const newName = `${req.file.filename}.${file}`;
+  const newPath = `${req.file.path}.${file}`;
+  fs.renameSync(req.file.path, newPath);
+
+  req.body.photo = newName;
+
   try {
     console.log(req.body)
     await create(req.body);
